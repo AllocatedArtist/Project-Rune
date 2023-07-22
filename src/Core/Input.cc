d1e190e9bbba3c4d4c8af4bc4a3f30527bc96e0c
Allocated Artist <Senorsiete2006@gmail.com>
2023-07-21 19:46:37 -0700

    Mouse options for input

--- src/Core/Input.cc
+++ src/Core/Input.cc
@@ -2,6 +2,14 @@
 
 std::array<int, GLFW_KEY_LAST> Input::keys_;
 
+float Input::mouse_x_ = 0.f;
+float Input::mouse_y_ = 0.f;
+float Input::mouse_delta_x_ = 0.f;
+float Input::mouse_delta_y_ = 0.f;
+bool Input::mouse_is_moving_ = false;
+Input::CursorState Input::cursor_state_ = Input::CursorState::kCursorStateNormal;
+
+
 void Input::SetKeyState(const int& key, const int& state) {
   keys_[key] = state;
 }
@@ -28,5 +36,55 @@ bool Input::IsKeyJustReleased(const int& key) {
     return true;
   }
   return false;
+}
+
+void Input::SetMouseStateX(const float& x) {
+  mouse_x_ = x;
+}
+
+void Input::SetMouseStateY(const float& y) {
+  mouse_y_ = y;
+}
+
+void Input::SetMouseStateDeltaX(const float& x) {
+  mouse_delta_x_ = x;
+}
+
+void Input::SetMouseStateDeltaY(const float& y) {
+  mouse_delta_y_ = y;
+}
+
+float Input::GetMouseX() {
+  return mouse_x_;
+}
+
+float Input::GetMouseY() {
+  return mouse_y_;
+}
+
+float Input::GetMouseDelta_X() {
+  return mouse_delta_x_;
+}
+
+float Input::GetMouseDelta_Y() {
+  return mouse_delta_y_;
+}
+
+void Input::SetMouseMoving(const bool& moving) {
+  mouse_is_moving_ = moving;
+}
+
+bool Input::IsMouseMoving() {
+  bool is_mouse_moving = mouse_is_moving_;
+  mouse_is_moving_ = false;
+  return is_mouse_moving;
+}
+
+void Input::SetCursorState(const Input::CursorState& cursor_state) {
+  cursor_state_ = cursor_state;
+}
+
+Input::CursorState Input::GetCursorState() {
+  return cursor_state_;
 }
 
--- src/Core/Input.h
+++ src/Core/Input.h
@@ -11,8 +11,37 @@ public:
   static bool IsKeyPressed(const int& key);
   static bool IsKeyUp(const int& key);
   static bool IsKeyJustReleased(const int& key);
+
+  static void SetMouseStateX(const float& x); //DOES NOT SET MOUSE POSITION
+  static void SetMouseStateY(const float& y); //IN THE WINDOW. ONLY USED FOR CALLBACK
+  static void SetMouseStateDeltaX(const float& x); //DOES NOT SET MOUSE DELTA
+  static void SetMouseStateDeltaY(const float& y); //USE FOR CALLBACK
+
+  //Don't use this. Only use for mouse callback
+  static void SetMouseMoving(const bool& moving); 
+
+  static float GetMouseX();
+  static float GetMouseY();
+  static float GetMouseDelta_X();
+  static float GetMouseDelta_Y();
+  static bool IsMouseMoving();
+
+  enum class CursorState {
+    kCursorStateNormal,
+    kCursorStateHidden,
+    kCursorStateDisabled,
+  };
+
+  static void SetCursorState(const CursorState& cursor_state);
+  static CursorState GetCursorState();
 private:
   static std::array<int, GLFW_KEY_LAST> keys_;
+  static float mouse_x_;
+  static float mouse_y_;
+  static float mouse_delta_x_;
+  static float mouse_delta_y_;
+  static bool mouse_is_moving_;
+  static CursorState cursor_state_;
 };
 
 
