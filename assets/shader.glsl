25c55a925b44d216278059ca20afd79ea3150791
Allocated Artist <Senorsiete2006@gmail.com>
2023-08-04 00:16:49 -0700

    use normal data for color

--- assets/shader.glsl
+++ assets/shader.glsl
@@ -1,14 +1,18 @@
 #vertex
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoords;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
 out vec2 fragTexCoords;
 
 uniform mat4 model;
 uniform mat4 viewProjection;
 
+out vec3 fragNormal;
+
 void main() {
+  fragNormal = aNormal;
   fragTexCoords = aTexCoords;
   gl_Position = viewProjection * model * vec4(aPos, 1.0);
 }
@@ -22,6 +26,7 @@ uniform sampler2D texture0;
 uniform vec3 fragBaseColor = vec3(0.0);
 
 in vec2 fragTexCoords;
+in vec3 fragNormal;
 
 void main() {
 
@@ -32,6 +37,6 @@ void main() {
   }
 
 
-  fragColor = texColor * vec4(fragBaseColor, 1.0);  
+  fragColor = texColor * vec4(fragBaseColor, 1.0) * vec4(fragNormal, 1.0);  
 }
 
