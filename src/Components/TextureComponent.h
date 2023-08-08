2b517bd7dbde2a91e367bf428d88d7ed8894ff99
Allocated Artist <Senorsiete2006@gmail.com>
2023-08-07 23:46:19 -0700

    still the same

--- src/Components/TextureComponent.h
+++ src/Components/TextureComponent.h
@@ -1,11 +1,12 @@
 #ifndef TEXTURE_COMPONENT_H_
 #define TEXTURE_COMPONENT_H_
 
+#include <memory>
+
 #include "../Graphics/Texture.h"
 
 struct TextureComponent {
   std::shared_ptr<Texture> texture_;
 };
-
 
 #endif
