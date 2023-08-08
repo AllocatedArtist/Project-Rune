eace34994d455cd5e5ee633303ad3f1b6571af85
Allocated Artist <Senorsiete2006@gmail.com>
2023-08-07 23:47:36 -0700

    Constructors to shorten code

--- src/Components/CameraComponent.h
+++ src/Components/CameraComponent.h
@@ -18,13 +18,30 @@ struct CameraComponent {
   glm::vec2 aspect_ratio_;
   glm::mat4 view_ = glm::mat4(1.0);
   glm::mat4 projection_ = glm::mat4(1.0);
+
+  CameraComponent(const glm::vec3& position, const float& fov, const float& near_clip, const float& far_clip) { 
+    forward_ = glm::vec3(0.f, 0.f, -1.f);
+    up_ = glm::vec3(0.f, 1.f, 0.f);
+    position_ = position;
+
+    fov_ = fov;
+    near_ = near_clip;
+    far_ = far_clip;
+  }
 };
 
+
 struct FlyCameraComponent {
-  float sensitivity_;
-  float speed_;
-  float yaw_;
-  float pitch_;
+  float sensitivity_ = 0.f;
+  float speed_ = 0.f;
+  float yaw_ = 0.f;
+  float pitch_ = 0.f;
+
+  FlyCameraComponent(const float& sensitivity, const float& speed) : sensitivity_(sensitivity), speed_(speed) {
+    yaw_ = 0.f;
+    pitch_ = 0.f;
+  }
 };
+
 
 #endif
